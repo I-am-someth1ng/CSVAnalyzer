5593882de1d69947ea6a74e40ea98b3f2a5943cf
Jacob Ingram <JacobIngram79@gmail.com>
2020-05-04 17:03:33 -0500

    Made skeletons for methods and the begining of javadocs

--- CSVAnalyzer.java
+++ CSVAnalyzer.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class CSVAnalyzer {
-
+/**
+ * This class aims to provide tools to help read .CSV files which are in a proper format.
+ * This is mostly just for me. 
+ * 
+ * @author Jacob Ingram
+ *
+ */
+public class CSVAnalyzer<T> 
+{
+	/**
+	 * Takes a .CSV file and puts it's contents all onto one line.
+	 * If removeCommas is true then each grouping of data will have one space inbetween them 
+	 * @return
+	 */
+	public static String FileToOneLine(File file, Boolean removeCommas)
+	{
+	
+		return "" ;
+	}
+	/**
+	 * 
+	 * @param file file to convert into an ArrayList
+	 * @return
+	 */
+	public static HashMap<Integer, String> FileToArrayList(File file)
+	{
+		
+		
+		
+	}
+	
 }
